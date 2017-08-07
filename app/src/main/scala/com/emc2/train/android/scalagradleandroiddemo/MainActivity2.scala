acfff8cf359a47af301aff81389c257aca46eb16
jessie <junchangchen@gmail.com>
2017-08-07 15:09:36 +0800

    scala

---
+++ app/src/main/scala/com/emc2/train/android/scalagradleandroiddemo/MainActivity2.scala
@@ -0,0 +1,73 @@
+package com.emc2.train.android.scalagradleandroiddemo
+
+import android.os.Bundle
+import android.support.design.widget.NavigationView.OnNavigationItemSelectedListener
+import android.support.design.widget.{FloatingActionButton, NavigationView, Snackbar}
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.{ActionBarDrawerToggle, AppCompatActivity}
+import android.support.v7.widget.Toolbar
+import android.view.{Menu, MenuItem, View}
+import org.scaloid.common._
+
+/**
+  * Main Activity
+  *
+  * Created by IT on 2017/8/7.
+  */
+class MainActivity2 extends AppCompatActivity with SActivity with OnNavigationItemSelectedListener {
+
+  override def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    val toolbar: Toolbar = find[Toolbar](R.id.toolbar)
+    setSupportActionBar(toolbar)
+
+    find[FloatingActionButton](R.id.fab).onClick((view: View) => {
+      Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+    })
+
+    val drawer: DrawerLayout = find[DrawerLayout](R.id.drawer_layout)
+    val toggle: ActionBarDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+    drawer.addDrawerListener(toggle)
+    toggle.syncState()
+
+    find[NavigationView](R.id.nav_view).setNavigationItemSelectedListener(this)
+  }
+
+  override def onBackPressed(): Unit = {
+    val drawer = find[DrawerLayout](R.id.drawer_layout)
+    if (drawer.isDrawerOpen(GravityCompat.START)) {
+      drawer.closeDrawer(GravityCompat.START)
+    } else {
+      super.onBackPressed()
+    }
+  }
+
+  override def onCreateOptionsMenu(menu: Menu): Boolean = {
+    getMenuInflater.inflate(R.menu.main, menu)
+    true
+  }
+
+  override def onOptionsItemSelected(item: MenuItem): Boolean = {
+    item.getItemId match {
+      case R.id.action_settings => return true
+    }
+    super.onOptionsItemSelected(item)
+  }
+
+  override def onNavigationItemSelected(item: MenuItem): Boolean = {
+    item.getItemId match {
+      case R.id.nav_camera =>
+      case R.id.nav_gallery =>
+      case R.id.nav_slideshow =>
+      case R.id.nav_manage =>
+      case R.id.nav_share =>
+      case R.id.nav_send =>
+    }
+
+    val drawer: DrawerLayout = find[DrawerLayout](R.id.drawer_layout)
+    drawer.closeDrawer(GravityCompat.START)
+    true
+  }
+}
